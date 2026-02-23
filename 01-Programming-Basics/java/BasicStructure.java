ad82f11272806b5506da824caeef92c57958ec2c
SujaanBhalla <bhallasujaan@gmail.com>
2026-02-23 16:17:59 +0000

    Fixed devcontainer configuration

---
+++ 01-Programming-Basics/java/BasicStructure.java
@@ -0,0 +1,12 @@
+// Basic Java Program Structure
+
+public class BasicStructure {
+
+    public static void main(String[] args) {
+
+        // Printing output
+        System.out.println("Hello Java World");
+
+    }
+
+}
